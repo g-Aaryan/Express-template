d786255a01c000c8b5fa20ec9ff897482197453c
g-Aaryan <guptaaaryan1012@gmail.com>
2026-01-11 21:11:25 +0530

    Added migrations in Hotel service

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,13 @@ type Serveconfig ={
     PORT:number
 }
 
+type DBConfig = {
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASSWORD: string,
+    DB_NAME: string
+}
+
 function loadenv(){
     dotenv.config();
     console.log("Env variables loaded")
@@ -13,3 +20,10 @@ loadenv();
 export const serverconfig:Serveconfig={
     PORT:Number(process.env.PORT)||3001
 }
+
+export const dbConfig: DBConfig = {
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_NAME: process.env.DB_NAME || 'test_db'
+};
